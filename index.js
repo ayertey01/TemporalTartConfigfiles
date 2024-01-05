8a57bd44e93bc6a06fc1490ec2aac7ce01fbb9c3
ayertey01 <pyterayertey@gmail.com>
2024-01-05 22:06:30 +0000

    error handling

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const fs = require("fs");
-// synchronous method 
-const textIn = fs.readFileSync("./txt/inputs.txt", "utf-8");
-console.log(textIn);
-const textOut =
-  "this is a script on modifying my mistake\n" +
-  textIn +
-  "Created on " +
-  Date.now();
+// const fs = require("fs");
+// // synchronous method 
+// const textIn = fs.readFileSync("./txt/inputs.txt", "utf-8");
+// console.log(textIn);
+// const textOut =
+//   "this is a script on modifying my mistake\n" +
+//   textIn +
+//   "Created on " +
+//   Date.now();
 
-fs.writeFileSync("./txt/output.txt", textOut);
-const peter = "ayertey01 ";
-console.log(`This was completed by ${peter}\n`);
+// fs.writeFileSync("./txt/output.txt", textOut);
+// const peter = "ayertey01 ";
+// console.log(`This was completed by ${peter}\n`);
 
-
+ 
 //asynchronous method  
 const fs = require("fs");
-fs.readFile("./txt/textfiles.txt", "utf-8", (err,data1)=>
-  {fs.readFile(`./txt/${data1}.txt`, "utf-8", (err,data2)=>{
+
+fs.readFile("./txt/textjfiles.txt", "utf-8", (err,data1)=>
+  {
+    if (err) return console.log(err);
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err,data2)=>{
     console.log(data2);
      fs.writeFile("./txt/finalrecords", `we combined ${data1}.\n and ${data2}`, (err)=>{
       console.log('compiled successfully'); 
